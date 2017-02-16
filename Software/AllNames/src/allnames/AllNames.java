7261dd14d00ef2e197ae54020d610f0da5173a09
Andreas Kucher <andreas.kucher@gmail.com>
2017-02-16 08:55:36 +0100

    Erste Version

---
+++ Software/AllNames/src/allnames/AllNames.java
@@ -0,0 +1,23 @@
+package allnames;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class AllNames {
+
+    public static void main(String[] args) {
+        
+        Set<String> myset = new TreeSet<String>();
+        
+        
+        // kommentar
+        myset.add(Andreas.getName());
+        
+        for(String s : myset) {
+            System.out.println(s);
+        }
+        
+        
+    }
+    
+}
